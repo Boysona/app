cab9456354c78d4cefe9b644fa81f0c3913d690b
BoySona <154109769+Boysona@users.noreply.github.com>
2025-09-22 18:26:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ BOT_TOKENS = [
     "8229893546:AAEOFMCw2q_iQu1sWKqygGNb5CiIOY8IUnY",
 ]
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyB1HVBY1a8XGE3bijTNJVBO1W759yK5KGc")
-WEBHOOK_BASE = os.environ.get("WEBHOOK_BASE", "https://top-selene-cadenuux57-5d5cdd61.koyeb.app/")
+WEBHOOK_BASE = os.environ.get("WEBHOOK_BASE", "top-selene-cadenuux57-5d5cdd61.koyeb.app/")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "6964068910"))
 SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-please-change")
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://hoskasii:GHyCdwpI0PvNuLTg@cluster0.dy7oe7t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
